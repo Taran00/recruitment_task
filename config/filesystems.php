715f3b5d3a8197b1f2185d6a54f35db58f099b7e
taran00 <michalt@osw.bydgoszcz.pl>
2019-07-02 21:36:30 +0200

    create filesystem for csv files

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,12 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'public_csv' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/csv',
+            'visibility' => 'public',
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
